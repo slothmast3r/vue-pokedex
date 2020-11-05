445944082db37d588bc16abc46301dbbbc59aa9f
Hubert Marzecki <h.marzecki@cheil.com>
2020-11-05 11:19:13 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <li v-for="poke in props.pokeList" :key="poke.url">
+                {{ poke.name }}
+            </li>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PokeList from './components/PokeList.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    PokeList
+  },
+  data() {
+    return {
+      pokeList : []
+    }
+  },
+   created() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.pokeList = response.data.resultes;
+      console.log(response.data)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+}
 }
 </script>
 
--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="hello">
+      <ul id="example-1">
+            <li v-for="poke in props.pokeList" :key="poke.url">
+                {{ poke.name }}
+            </li>
+</ul>
   </div>
 </template>
 
@@ -7,25 +12,12 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    pokeList: Array
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
